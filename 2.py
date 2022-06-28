6ff5d8aba4b7cfd74c6bfd819b7a40f282113383
KristinaGolushkova <107718947+KristinaGolushkova@users.noreply.github.com>
2022-06-28 23:39:58 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -6,10 +6,10 @@ str2 = input(" введите строку : ")
 if(len(str1) == len(str2)):
 
     # Сортируем обе строки
-    sorted_str1 = sorted(str1)
-    sorted_str2 = sorted(str2)
+    sorstr1 = sorted(str1)
+    sorstr2 = sorted(str2)
 
-    if(sorted_str1 == sorted_str2):
+    if(sorstr1 == sorstr2):
         print(str1 + " и " + str2 + " Являются анаграмой.")
 
 
